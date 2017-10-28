45cc6d5a03f04955acedbbff4b45a69861b3d0fa
Gabriel Bessler <gabrielbessler@gmail.com>
2017-10-28 05:29:04 -0700

    Undo

--- venv/static/Main.js
+++ venv/static/Main.js
@@ -79,10 +79,7 @@ function getScore() {
 
             } else if (data == -1) {
                 document.getElementById("timer").innerHTML = 0 + " s";
-                if (data[2] == data[0]) {
-                    document.getElementById("score1").innerHTML = "DRAW";
-                    document.getElementById("score2").innerHTML = "DRAW";
-                } else if (data[2] < data[0]) {
+                if (data[2] < data[0]) {
                     document.getElementById("score1").innerHTML = "WINNER";
                     document.getElementById("score2").innerHTML = "LOSER";
                 } else {
