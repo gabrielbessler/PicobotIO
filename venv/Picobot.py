0b248213ced770bd947a5c8dd5c8b389023c3157
ssxtan <samuelsxtan@gmail.com>
2017-10-27 21:34:56 -0700

    fixed Picobot.py

--- venv/Picobot.py
+++ venv/Picobot.py
@@ -4,9 +4,17 @@ def Picobot(object):
 
     # constructor
     def __init__(xPos, yPos, rules):
-        self.xPos = xPox
+        self.xPos = xPos
         self.yPos = yPos
         self.ruleList = rules
         self.currentState = 0
+    
+    def getDir(surroundings):
+        for rule in rules:
+            if self.currentState == rule[0] and surroundings == rule[1]:
+                self.currentState = rule[3]
+                return rule[2]
+        return
+        
 
 
