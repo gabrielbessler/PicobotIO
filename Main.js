8262070db0b538e96592926fa48351a9613f89eb
Gabriel Bessler <gabrielbessler@gmail.com>
2017-10-27 20:10:32 -0700

    jake

---
+++ Main.js
@@ -0,0 +1,8 @@
+function drawMap(map) {
+    /* Takes a map as a javascript array and draws the map on the screen */
+    for (var row = 0; row < sampleMap.length(); i++) {
+
+    }
+}
+
+sampleMap = [[0,0,0], [0,0,0], [0,0,0]]
