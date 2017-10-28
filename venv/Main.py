37433bf30509b6d409f55ca2de42f141c731a8a5
Gabriel Bessler <gabrielbessler@gmail.com>
2017-10-27 21:18:48 -0700

    Updated Files

--- venv/Main.py
+++ venv/Main.py
@@ -2,5 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("Main.html", games = [[0,1], [1,2], [2,0]])
+
+@app.route('/game/<int:game_num>')
+def join_game(game_num):
+    '''
+    Join game number <game_num> given in the URL
+    '''
+    return render_template("Game.html")
